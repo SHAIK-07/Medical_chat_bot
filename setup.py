388f3dfd3c1440a7675ec43738c06f2e98373c78
SHAIK-07 <shaikhidaythulla07@gmail.com>
2025-03-03 18:58:32 +0530

    Version 1

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Generate AI Project',
+    version = '0.1',
+    packages = find_packages(),
+    author='Shaik Hidaythulla',
+    author_email='shaik.hidaythulla07@gmail.com',
+    description='A simple python package to generate a template for AI project',
+    install_requires=[ ]
+)
